b36334f4d796da10cf17c61b61892983cdd1527c
Larica <larica.bawar@gmail.com>
2015-11-01 21:53:21 +0800

    -a

--- node.js/objects.js
+++ node.js/objects.js
@@ -1,15 +1,11 @@
-var getDieRoll = function(dieSize){
-	var result = Math.ceil (dieSize* Math.random());
-	return result;
-};
-
 var die = {
-	size: 5
+	size: 5,
+	count: 1,
+	roll: function(dieSize){
+		var result = Math.ceil (dieSize * Math.random());
+		return result;
+	}
 };
 
 console.log(die);
-console.log(die.size);
-
-die.size = 6;
-console.log(die);
-console.log(die.size);
+console.log(die.roll(6));
